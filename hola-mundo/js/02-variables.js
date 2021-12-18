043aea36b9186950af142319e832d1359b2f391d
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-12-18 17:23:51 +0100

    Constantes

--- hola-mundo/js/02-variables.js
+++ hola-mundo/js/02-variables.js
@@ -1,3 +1,5 @@
+'use strict'
+
 // VARIABLES
 // Una variable es un contenedor de información
 
@@ -5,6 +7,8 @@ var pais = 'España'
 var continente = 'Europa'
 var antiguedad = 2019
 var pais_y_continente = pais + ` ` + continente
+let prueba = 'hola'
+alert('hola')
 
 pais = 'Mexico'
 continente = 'Latinoamerica'
